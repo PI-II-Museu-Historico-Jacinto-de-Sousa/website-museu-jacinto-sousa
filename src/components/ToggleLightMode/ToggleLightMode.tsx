395c39077f4bb1afa8f978e1608780653d153882
João Pedro Pereira Holanda <joaopedroph.dev@gmail.com>
2024-04-12 17:40:40 -0300

    Config/tests (#25)
    * adicionando configuracoes de teste e exemplo de teste e2e
    
    * movendo componente, adicionando testes de componente e renomeando theme
    
    * configurando workflow para testes de componente
    
    * incluindo execuções em pull requests

--- src/components/ToggleLightMode/ToggleLightMode.tsx
+++ src/components/ToggleLightMode/ToggleLightMode.tsx
@@ -1,12 +1,13 @@
-import { IconButton } from "@mui/material";
+import { IconButton, PaletteMode } from "@mui/material";
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 
-const ToggleLightMode = ({colorMode, mode}: {colorMode: any, mode: string}) => {
+
+const ToggleLightMode = ({ colorMode, mode }: { colorMode: { toggleColorMode: () => void; }, mode: PaletteMode }) => {
     return (
         <>
             <IconButton onClick={colorMode.toggleColorMode}>
-            {mode === 'dark' ? <DarkModeIcon /> : <LightModeIcon/>}
+                {mode === 'dark' ? <DarkModeIcon /> : <LightModeIcon />}
             </IconButton>
         </>
     )
