91591ff2afc057cde3e3a97058d47e85c1465373
Erick <erickferreiragaspar@gmail.com>
2024-04-13 17:50:13 -0300

    Delete Footer.css
    Update Footer.tsx with styled components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,33 +1,93 @@
-import './Footer.css' 
 import telephoneIcon from "../assets/telephone.svg"
 import whatsIcon from "../assets/whatsapp.svg"
 import emailIcon from "../assets/email.svg"
 import addressIcon from "../assets/address.svg"
+import { styled } from '@mui/material'
 
-const Footer = ({telephone, whatsapp, email, address}: {telephone: string, whatsapp: string, email: string, address: string}) => {
+const FooterContainer = styled('div')`
+    position: absolute;
+    height: fit-content;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    transform: translateX(-50%);
+    bottom: 0;
+`
+const FooterContent = styled('div')`
+    position: relative;
+    top: 0;
+    display: flex;
+    width: 100%;
+    height: 9%;
+    justify-content: space-around;
+    align-items: center;
+    flex-shrink: 0;
+    background: var(--Schemes-Outline, #85736C);
+`
+
+const FooterItem = styled('li')`
+    list-style-type: none;
+    font-size: 2.5vh;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    padding: 1.5vh;
+`
+
+const FooterIcon = styled('img')`
+    position: relative;
+    margin-right: 1vw;
+`
+
+const FooterEditable = styled('div')`
+    position: relative;
+    width: 100%;
+    height: 10%;
+    bottom: 0;
+    background: var(--Schemes-Outline, #85736C);
+`
+
+const Footer = ({telephone, whatsapp, email, address, editable}: {telephone: string, whatsapp: string, email: string, address: string, editable: boolean}) => {
     return(
         <>
-            <div id="footer-content">
-                <li className = 'Footer-item'>
-                    <img src={telephoneIcon} alt="" className='Footer-icon'/>
-                    {telephone}
-                </li>
+            <FooterContainer>
+                <FooterContent style={{}}>
+                    <FooterItem>
+                        <FooterIcon src={telephoneIcon}/>
+                        {telephone}
+                    </FooterItem>
 
-                <li className = 'Footer-item'>
-                    <img src={whatsIcon} alt="" className='Footer-icon'/>
-                    {whatsapp}
-                </li>
+                    <FooterItem>
+                        <FooterIcon src={whatsIcon}/>
+                        {whatsapp}
+                    </FooterItem>
 
-                <li className = 'Footer-item'>
-                    <img src={emailIcon} alt="" className='Footer-icon'/>
-                    {email}
-                </li>
+                    <FooterItem>
+                        <FooterIcon src={emailIcon}/>
+                        {email}
+                    </FooterItem>
 
-                <li className = 'Footer-item'>
-                    <img src={addressIcon} alt="" className='Footer-icon'/>
-                    {address}
-                </li>
-            </div>
+                    <FooterItem>
+                        <FooterIcon src={addressIcon}/>
+                        {address}
+                    </FooterItem>
+                </FooterContent>
+
+                {
+                    editable === true ?
+
+                    <FooterEditable>
+                        <strong style={{cursor: "pointer"}}>(Editar)</strong>
+                    </FooterEditable>
+
+                    :
+
+                    <></>
+                }
+
+            </FooterContainer>
         </>
     )
 }
