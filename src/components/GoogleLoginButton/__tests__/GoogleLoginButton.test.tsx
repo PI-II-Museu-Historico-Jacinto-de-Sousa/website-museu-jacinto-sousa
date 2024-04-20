b6fed492ee15bacaca4931d3be885f07741fd09b
Francisco Paulino Arruda FIlho <122313737+Francisco-Paulino-Arruda-Filho@users.noreply.github.com>
2024-04-20 17:22:19 -0300

    Criando googleLoginButton (#44)
    * Criando googleLoginButton
    
    * Criando testes, inserindo imagem do ícone do google abrindo popup
    
    * Mudando local da imagem e estilizando da maneira certa
    
    * Testando usado useTheme
    
    * Corrigindo testes e exportanto a função para a pasta utils
    
    * Tirando função de teste

---
+++ src/components/GoogleLoginButton/__tests__/GoogleLoginButton.test.tsx
@@ -0,0 +1,21 @@
+import GoogleLoginButton from "../GoogleLoginButton"
+import getDesignTokens from "../../../theme/theme"
+import { ThemeProvider } from "@emotion/react"
+import {loginMethods} from "../../../Utils/loginGoogle"
+
+const theme = getDesignTokens('light')
+
+describe("Testando o componente GoogleLoginButton", () => {
+    it("renderiza corretamente", () => {
+        cy.mount(<ThemeProvider theme={theme}><GoogleLoginButton data-class-ref='button' /></ThemeProvider>)
+        cy.get("[data-cy='botaoLoginGoogle']").should('exist')
+    })
+
+    it("Checa se a função é chamada", () => {
+        cy.mount(<ThemeProvider theme={theme}><GoogleLoginButton data-class-ref='button' /></ThemeProvider>)
+        cy.spy(loginMethods, 'loginGoogle')
+        cy.get("[data-cy='botaoLoginGoogle']").click().then(() => {
+            expect(loginMethods.loginGoogle).to.be.called  
+        })
+    })
+})
