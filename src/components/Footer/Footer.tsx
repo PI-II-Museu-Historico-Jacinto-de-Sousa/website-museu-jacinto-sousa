7f3d4eec8611c4f931f7ee5955c157c3c13644eb
João Pedro Pereira Holanda <joaopedroph.dev@gmail.com>
2024-04-27 14:43:42 -0300

    fix: removendo posicionamento absoluto do footer (#68)
    * fix: removendo posicionamento absoluto do footer
    
    removendo atributos css position, transform, left e bottom de FooterContainer
    
    * chore: posicionando footer no fundo mesmo com a página vazia
    
    Adicionando flex a div root em index.html
    configurando div root para preencher o viewport
    adicionando margem automatica ao footer
    
    * chore: fixando footer no topo mesmo com elementos definidos depois
    
    adicionando posicao relativa ao body em index.html
    revertendo posicao do footer para absoluta

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,259 +8,258 @@ import { app } from '../../../firebase/firebase'
 import { useEffect, useState } from 'react'
 import { collection, doc, getDocs, getFirestore, setDoc } from 'firebase/firestore'
 
-interface IFooterData{
-    telephone: string,
-    whatsapp: string,
-    email: string,
-    address: string
+interface IFooterData {
+  telephone: string,
+  whatsapp: string,
+  email: string,
+  address: string
 }
 
 const Footer = () => {
 
-    //states of component
+  //states of component
 
-    const [edit, setEdit] = useState(false)
+  const [edit, setEdit] = useState(false)
 
-    const [logged, setLogged] = useState(false)
-    
-    const [footerData, setFooterData] = useState<IFooterData>({
-        address: '',
-        email: '',
-        telephone: '',
-        whatsapp: ''
+  const [logged, setLogged] = useState(false)
+
+  const [footerData, setFooterData] = useState<IFooterData>({
+    address: '',
+    email: '',
+    telephone: '',
+    whatsapp: ''
+  })
+
+  const [newTelephone, setNewTelephone] = useState(footerData.telephone)
+  const [newWhatsapp, setNewWhatsapp] = useState(footerData.whatsapp)
+  const [newEmail, setNewEmail] = useState(footerData.email)
+  const [newAddress, setNewAddress] = useState(footerData.address)
+
+  const startEdit = () => {
+    setNewTelephone(footerData.telephone)
+    setNewWhatsapp(footerData.whatsapp)
+    setNewEmail(footerData.email)
+    setNewAddress(footerData.address)
+
+    setEdit(true)
+  }
+
+  const cancelEdit = () => {
+    setEdit(false)
+  }
+
+  const applyEdit = async () => {
+    const newFooterData = {
+      address: newAddress,
+      email: newEmail,
+      telephone: newTelephone,
+      whatsapp: newWhatsapp
+    }
+
+    console.log(newFooterData)
+    try {
+      await submitToFirestore(newFooterData)
+      setEdit(false)
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  //comunicate with firestore
+
+  const getFooterData = async () => {
+    try {
+      const db = getFirestore()
+      const queryFooter = collection(db, "informações-museu")
+      const collections = await getDocs(queryFooter)
+      const data = collections.docs.map(doc => doc.data())[0]
+
+      setFooterData(data as IFooterData)
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  const submitToFirestore = async (newFooterData: IFooterData) => {
+    try {
+      const db = getFirestore()
+      await setDoc(doc(db, "informações-museu", "footer"), newFooterData)
+
+      setFooterData(newFooterData)
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  //render time
+
+  useEffect(() => {
+    const auth = getAuth(app)
+
+    auth.onAuthStateChanged((user) => {
+      setLogged(user ? true : false)
     })
 
-    const [newTelephone, setNewTelephone] = useState(footerData.telephone)
-    const [newWhatsapp, setNewWhatsapp] = useState(footerData.whatsapp)
-    const [newEmail, setNewEmail] = useState(footerData.email)
-    const [newAddress, setNewAddress] = useState(footerData.address)
+    getFooterData()
+  }, [])
 
-    const startEdit = () =>{
-        setNewTelephone(footerData.telephone)
-        setNewWhatsapp(footerData.whatsapp)
-        setNewEmail(footerData.email)
-        setNewAddress(footerData.address)
+  return (
+    <FooterContainer>
+      {edit ?
 
-        setEdit(true)
-    }
-    
-    const cancelEdit = () =>{
-        setEdit(false)
-    }
-    
-    const applyEdit = async () =>{
-        const newFooterData = {
-            address: newAddress,
-            email: newEmail,
-            telephone: newTelephone,
-            whatsapp: newWhatsapp
-        }
-        
-        console.log(newFooterData)
-        try{
-            await submitToFirestore(newFooterData)
-            setEdit(false)
-        }
-        catch(error){
-            console.log(error)
-        }
-    }
+        // component apply to edition
 
-    //comunicate with firestore
-    
-    const getFooterData = async () =>{
-        try {
-            const db = getFirestore()
-            const queryFooter = collection(db, "informações-museu")
-            const collections = await getDocs(queryFooter)
-            const data = collections.docs.map(doc => doc.data())[0]
+        <>
+          <FooterContent>
+            <FooterItem>
+              <PhoneIcon sx={{ fontSize: '3vh', marginRight: '1vw' }} />
+              <TextField type='text' value={newTelephone} onChange={(e) => setNewTelephone(e.target.value)} />
+            </FooterItem>
 
-            setFooterData(data as IFooterData)
-        } 
-        catch(error){
-            console.log(error)
-        }
-    }
-    
-    const submitToFirestore = async (newFooterData: IFooterData) =>{
-        try{
-            const db = getFirestore()
-            await setDoc(doc(db, "informações-museu", "footer"), newFooterData)
+            <FooterItem>
+              <WhatsAppIcon sx={{ fontSize: '3vh', marginRight: '1vw' }} />
+              <TextField type='text' value={newWhatsapp} onChange={(e) => setNewWhatsapp(e.target.value)} />
+            </FooterItem>
 
-            setFooterData(newFooterData)
-        }
-        catch(error){
-            console.log(error)
-        }
-    }
-    
-    //render time
-    
-    useEffect(() => {
-        const auth = getAuth(app)
-        
-        auth.onAuthStateChanged((user) => {
-            setLogged(user ? true : false)
-        })
-        
-        getFooterData()
-    }, [])
-    
-    return (
-        <FooterContainer>
-            { edit ?
-            
-            // component apply to edition
+            <FooterItem>
+              <EmailIcon sx={{ fontSize: '3vh', marginRight: '1vw' }} />
+              <TextField type='text' value={newEmail} onChange={(e) => setNewEmail(e.target.value)} />
+            </FooterItem>
 
-            <>
-                <FooterContent>
-                    <FooterItem>
-                        <PhoneIcon sx={{fontSize: '3vh', marginRight: '1vw'}}/>
-                        <TextField type='text' value={newTelephone} onChange={(e) => setNewTelephone(e.target.value)}/>
-                    </FooterItem>
+            <FooterItem>
+              <PlaceIcon sx={{ fontSize: '3vh', marginRight: '1vw' }} />
+              <TextField type='text' value={newAddress} onChange={(e) => setNewAddress(e.target.value)} />
+            </FooterItem>
+          </FooterContent>
 
-                    <FooterItem>
-                        <WhatsAppIcon sx={{fontSize: '3vh', marginRight: '1vw'}}/>
-                        <TextField type='text' value={newWhatsapp} onChange={(e) => setNewWhatsapp(e.target.value)}/>
-                    </FooterItem>
+          <FooterEditable>
+            <EditableButton onClick={() => applyEdit()}><strong>(Salvar)</strong></EditableButton>
 
-                    <FooterItem>
-                        <EmailIcon sx={{fontSize: '3vh', marginRight: '1vw'}}/>
-                        <TextField type='text' value={newEmail} onChange={(e) => setNewEmail(e.target.value)}/>
-                    </FooterItem>
+            <EditableButton onClick={() => cancelEdit()}><strong>(Cancelar)</strong></EditableButton>
+          </FooterEditable>
+        </>
 
-                    <FooterItem>
-                        <PlaceIcon sx={{fontSize: '3vh', marginRight: '1vw'}}/>
-                        <TextField type='text' value={newAddress} onChange={(e) => setNewAddress(e.target.value)}/>
-                    </FooterItem>
-                </FooterContent>
+        :
 
-                <FooterEditable>
-                    <EditableButton onClick={() => applyEdit()}><strong>(Salvar)</strong></EditableButton>
+        // component out of edition
 
-                    <EditableButton onClick={() => cancelEdit()}><strong>(Cancelar)</strong></EditableButton>
-                </FooterEditable>
-            </>
+        <>
+          <FooterContent>
+            <FooterItem>
+              <PhoneIcon sx={{ fontSize: '3vh', marginRight: '1vw' }} />
+              {footerData.telephone}
+            </FooterItem>
 
-            :
+            <FooterItem>
+              <WhatsAppIcon sx={{ fontSize: '3vh', marginRight: '1vw' }} />
+              {footerData.whatsapp}
+            </FooterItem>
 
-            // component out of edition
-            
-            <>
-                <FooterContent>
-                    <FooterItem>
-                        <PhoneIcon sx={{fontSize: '3vh', marginRight: '1vw'}}/>
-                        {footerData.telephone}
-                    </FooterItem>
+            <FooterItem>
+              <EmailIcon sx={{ fontSize: '3vh', marginRight: '1vw' }} />
+              {footerData.email}
+            </FooterItem>
 
-                    <FooterItem>
-                        <WhatsAppIcon sx={{fontSize: '3vh', marginRight: '1vw'}} />
-                        {footerData.whatsapp}
-                    </FooterItem>
+            <FooterItem>
+              <PlaceIcon sx={{ fontSize: '3vh', marginRight: '1vw' }} />
+              {footerData.address}
+            </FooterItem>
+          </FooterContent>
 
-                    <FooterItem>
-                        <EmailIcon sx={{fontSize: '3vh', marginRight: '1vw'}} />
-                        {footerData.email}
-                    </FooterItem>
+          {
+            logged ?
 
-                    <FooterItem>
-                        <PlaceIcon sx={{fontSize: '3vh', marginRight: '1vw'}} />
-                        {footerData.address}
-                    </FooterItem>
-                </FooterContent>
+              <FooterEditable>
+                <EditableButton onClick={() => startEdit()}><strong>(Editar)</strong></EditableButton>
+              </FooterEditable>
+              :
+              ''
+          }
 
-                {
-                    logged ?
-
-                    <FooterEditable>
-                        <EditableButton onClick={() => startEdit()}><strong>(Editar)</strong></EditableButton>
-                    </FooterEditable>
-                    :
-                    ''
-                }
-
-            </>
-            }
-        </ FooterContainer>
-    )
+        </>
+      }
+    </ FooterContainer>
+  )
 }
 
-const FooterContainer = styled('footer')(({theme}: {theme:Theme}) =>({
-    position: 'absolute',
-    height: 'fit-content',
-    width: '100%',
-    display: 'flex',
+const FooterContainer = styled('footer')(({ theme }: { theme: Theme }) => ({
+  position: 'absolute',
+  left: 0,
+  bottom: 0,
+  height: 'fit-content',
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignContent: 'center',
+  flexWrap: 'wrap',
+  backgroundColor: theme.palette.outline.main
+}))
+
+const FooterContent = styled('div')(() => ({
+  position: 'relative',
+  top: '0',
+  display: 'flex',
+  width: '100%',
+  height: '9%',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  flexShrink: '0',
+
+  '@media screen and (max-width: 600px)': {
     flexDirection: 'column',
-    justifyContent: 'center',
-    alignContent: 'center',
-    transform: 'translateX(-50%)',
-    left: '50%',
-    bottom: '0',
-    flexWrap: 'wrap',
-    backgroundColor: theme.palette.outline.main
+    height: 'auto'
+  }
 }))
 
-const FooterContent = styled('div')(() =>({
-    position: 'relative',
-    top: '0',
-    display: 'flex',
-    width: '100%',
-    height: '9%',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    flexShrink: '0',
-
-    '@media screen and (max-width: 600px)':{
-        flexDirection: 'column',
-        height: 'auto'
-    }
+const FooterItem = styled('li')(({ theme }: { theme: Theme }) => ({
+  listStyleType: 'none',
+  fontSize: '2.5vh',
+  display: 'flex',
+  justifyContent: 'start',
+  alignItems: 'center',
+  padding: '1.5vh',
+  color: theme.palette.onSurface.main
 }))
 
-const FooterItem = styled('li')(({theme}: {theme:Theme}) =>({
-    listStyleType: 'none',
-    fontSize: '2.5vh',
-    display: 'flex',
-    justifyContent: 'start',
-    alignItems: 'center',
-    padding: '1.5vh',
-    color: theme.palette.onSurface.main
+const FooterEditable = styled('div')(({ theme }: { theme: Theme }) => ({
+  position: 'relative',
+  width: '100%',
+  height: '10%',
+  bottom: '0',
+  justifyContent: 'center',
+  alignContent: 'center',
+  display: 'flex',
+  color: theme.palette.onSurface.main
 }))
 
-const FooterEditable = styled('div')(({theme}: {theme:Theme}) =>({
-    position: 'relative',
-    width: '100%',
-    height: '10%',
-    bottom: '0',
-    justifyContent: 'center',
-    alignContent: 'center',
-    display: 'flex',
-    color: theme.palette.onSurface.main
-}))
-
-const EditableButton = styled('button')(({theme}: {theme:Theme}) =>({
-    cursor: 'pointer',
-    background: 'none',
-    border: 'none',
-    fontSize: '2.5vh',
-    color: theme.palette.onSurface.main
+const EditableButton = styled('button')(({ theme }: { theme: Theme }) => ({
+  cursor: 'pointer',
+  background: 'none',
+  border: 'none',
+  fontSize: '2.5vh',
+  color: theme.palette.onSurface.main
 }))
 
 const TextField = styled('input')(() => ({
-    backgroundColor: 'white',
-    color: 'black',
-    borderStyle: 'hidden',
-    outline: 'none',
-    padding: '1vh',
-    fontSize: '2vh',
-    
-    '@media screen and (min-width: 600px)':{
-        width: '15vw'
-    },
+  backgroundColor: 'white',
+  color: 'black',
+  borderStyle: 'hidden',
+  outline: 'none',
+  padding: '1vh',
+  fontSize: '2vh',
 
-    '@media screen and (max-width: 600px)':{
-        with: '50vw',
-        height: 'auto'
-    }
+  '@media screen and (min-width: 600px)': {
+    width: '15vw'
+  },
+
+  '@media screen and (max-width: 600px)': {
+    with: '50vw',
+    height: 'auto'
+  }
 }))
 
 export default Footer
