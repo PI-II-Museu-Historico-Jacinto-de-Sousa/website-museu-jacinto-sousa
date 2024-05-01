28e1e5a9c27d106fc374881e1f2231dd7b810786
kaioportela <kaioportela@au.ufc.br>
2024-05-01 09:57:08 -0300

    Tirando componente do App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Home from './pages/Home';
 import getDesignTokens from './theme/theme';
 import Footer from './components/Footer/Footer';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
-import SnippetMaps from './components/SnippetMaps/SnippetMaps';
+
 
 function App() {
 
@@ -33,9 +33,7 @@ function App() {
       <BrowserRouter>
         <ThemeProvider theme={theme}> {/*Para aplicar o tema*/}
           <CssBaseline>
-            <SnippetMaps URL="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.7683535114555!2d-39.013285085532314!3d-4.971648897769986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7aef39332e0fc39%3A0xd69d26b774a16276!2sMuseu%20Jacinto%20de%20Sousa!5e0!3m2!1spt-BR!2sbr!4v1650815010175!5m2!1spt-BR!2sbr">
-              
-            </SnippetMaps>
+            
             <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={'pt-br'}> {/*Contexto para localizacao de componentes material*/}
               <main style={{ flexGrow: 1 }}> {/*Conteúdo principal renderizado em uma rota*/}
                 <Routes>
