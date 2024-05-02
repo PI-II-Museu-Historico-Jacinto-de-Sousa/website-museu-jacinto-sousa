05de1155f563baf5dbc2c8aaf7f084a2f89e3f97
Francisco Paulino Arruda Filho <fpaulinofilho04@gmail.com>
2024-05-02 17:42:37 -0300

    Checagem de undefined no hook

--- src/hooks/useInfoMuseuForm.ts
+++ src/hooks/useInfoMuseuForm.ts
@@ -7,7 +7,21 @@ const useInfoMuseuForm = (defaultValues?: InfoMuseu): {
   handleSubmit: <T>(callback: (data: InfoMuseu) => Promise<void>) => (event: React.SyntheticEvent) => void;
   formState: FormState<InfoMuseu>;
 } => {
-  const { register, control, handleSubmit, formState } = useForm<InfoMuseu>({defaultValues});
+  // Verifique se defaultValues é undefined e atribua valores padrão apropriados
+  if (defaultValues === undefined) {
+    defaultValues = {
+      nome: '',
+      texto: '',
+      //Não é possível definir um objeto vazio como valor padrão para imagem, pois ele contém propriedades obrigatórias
+      imagem: {
+        src: '',
+        title: '',
+        alt: '',
+      },
+    };
+  }
+
+  const { register, control, handleSubmit, formState } = useForm<InfoMuseu>({ defaultValues });
 
   return { register, control, handleSubmit, formState };
 }
