9de046385e914556a3f552309d277bba5c0bd13a
Francisco Paulino Arruda Filho <fpaulinofilho04@gmail.com>
2024-05-02 14:49:43 -0300

    Deixando parâmetro opcional

--- src/hooks/useInfoMuseuForm.ts
+++ src/hooks/useInfoMuseuForm.ts
@@ -1,13 +1,15 @@
 import { FormState, useForm, Control, UseFormRegister } from 'react-hook-form';
 import { InfoMuseu } from '../interfaces/InfoMuseu';
 
-export default function useInfoMuseuForm(defaultValues: InfoMuseu): {
+const useInfoMuseuForm = (defaultValues?: InfoMuseu): {
   register: UseFormRegister<InfoMuseu>;
   control: Control<InfoMuseu>;
   handleSubmit: <T>(callback: (data: InfoMuseu) => Promise<void>) => (event: React.SyntheticEvent) => void;
   formState: FormState<InfoMuseu>;
-} {
-  const { register, control, handleSubmit, formState } = useForm<InfoMuseu>({ defaultValues });
+} => {
+  const { register, control, handleSubmit, formState } = useForm<InfoMuseu>({defaultValues});
 
   return { register, control, handleSubmit, formState };
 }
+
+export default useInfoMuseuForm;
