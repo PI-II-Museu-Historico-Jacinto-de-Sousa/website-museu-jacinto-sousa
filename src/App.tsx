2e7a98125d293f47754bdee9b2f89f13978a3ab5
Joao-Pedro-P-Holanda <joaopedroph.dev@gmail.com>
2024-05-27 21:15:06 -0300

    chore: substituindo conteúdo em App.tsx por RouteProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,11 @@
-import { ThemeProvider } from '@emotion/react';
-import { CssBaseline, PaletteMode, ThemeOptions, createTheme, responsiveFontSizes } from '@mui/material';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import 'dayjs/locale/pt-br';
-import { useMemo, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import CriarItemAcervo from './pages/CriarItemAcervo';
-import Home from './pages/Home';
-import getDesignTokens from './theme/theme';
-import Footer from './components/Footer/Footer';
-import ScrollToTop from './components/ScrollToTop/ScrollToTop';
-import NavBar from './components/NavBar/NavBar';
+import { RouterProvider } from 'react-router-dom';
+import router from './router/router';
 
 
 function App() {
-
-  const [mode, setMode] = useState<PaletteMode>('light'); //estado do tema escolhido
-
-  const colorMode = useMemo( //função para mudar o tema, passada para o componente ToggleLightMode
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode: PaletteMode) =>
-          prevMode === 'light' ? 'dark' : 'light',
-        );
-      },
-    }),
-    [],
-  );
-
-  const theme = useMemo(() => responsiveFontSizes(createTheme(getDesignTokens(mode) as ThemeOptions)), [mode]); //quando o modo selecionado muda, o tema é atualizado
   return (
-    <>
-      <BrowserRouter>
-        <ThemeProvider theme={theme}> {/*Para aplicar o tema*/}
-          <CssBaseline>
-            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={'pt-br'}> {/*Contexto para localizacao de componentes material*/}
-                <NavBar colorMode={colorMode} mode={mode}/>
-              <main style={{ flexGrow: 1 }}> {/*Conteúdo principal renderizado em uma rota*/}
-              <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/acervo/criar-item" element={<CriarItemAcervo />} />
-                </Routes>
-              </main>
-              <ScrollToTop />
-              <Footer />
-            </LocalizationProvider>
-          </CssBaseline>
-        </ThemeProvider>
-      </BrowserRouter>
-    </>
+    <RouterProvider router={router} />
   );
 
 }
