ffe21ddff54e93962ff3ec83246a092f5c595c1d
Joao-Pedro-P-Holanda <joaopedroph.dev@gmail.com>
2024-05-28 21:23:45 -0300

    ci: criando workflow de build do projeto
    definindo workflow com steps para salvar o bundle como artefato e executar reports do lighthouse
    
    Adicionando arquivo de configuração do lighthouse que define o comando para executar servidor preview. Ignorando as asserções (provisório)
    
    adicionando dependência vite-bundle-visualizer
    
    ignorando pasta reports

---
+++ .lighthouserc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ["http://localhost:4173/"],
+      startServerCommand: "npm run preview",
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["off"],
+        "categories:accessibility": ["off"],
+        "best-practices": ["off"],
+        "categories:seo": ["off"],
+      },
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
