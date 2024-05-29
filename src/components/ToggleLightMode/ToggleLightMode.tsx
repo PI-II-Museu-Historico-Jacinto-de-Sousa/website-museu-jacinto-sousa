398edda9d136f5e5c59b80b20b4ceb92c2a830d8
Francisco Paulino Arruda Filho <fpaulinofilho04@gmail.com>
2024-05-29 15:14:56 -0300

    Criando função de localStorage

--- src/components/ToggleLightMode/ToggleLightMode.tsx
+++ src/components/ToggleLightMode/ToggleLightMode.tsx
@@ -2,8 +2,14 @@ import { IconButton, PaletteMode } from "@mui/material";
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 
+const saveDateLocalStorage = ({ colorMode, mode }: { colorMode: { toggleColorMode: () => void; }, mode: PaletteMode }) => {
+    localStorage.setItem('colorMode', mode);
+    colorMode.toggleColorMode();
+    console.log('colorMode', mode);
+}
 
 const ToggleLightMode = ({ colorMode, mode }: { colorMode: { toggleColorMode: () => void; }, mode: PaletteMode }) => {
+  saveDateLocalStorage({ colorMode, mode });
     return (
         <>
             <IconButton onClick={colorMode.toggleColorMode}>
