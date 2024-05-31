7d3a1908487aad9a9e9c13c57752b93fec75f9e3
Francisco Paulino Arruda FIlho <122313737+Francisco-Paulino-Arruda-Filho@users.noreply.github.com>
2024-05-31 15:08:20 -0300

    Feat/salvar tema (#126)
    * Criando função de localStorage
    
    * Recuperando o mode do localStorage
    
    * Revertendo mudanças na navBar, salvando colorMode em localStorage e chamando no hook no Root.tsx
    
    * Removendo console.log
    
    * Removendo estados descessário e trocando nome de outro estado definido om useMemo

--- src/components/ToggleLightMode/ToggleLightMode.tsx
+++ src/components/ToggleLightMode/ToggleLightMode.tsx
@@ -2,7 +2,6 @@ import { IconButton, PaletteMode } from "@mui/material";
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 
-
 const ToggleLightMode = ({ colorMode, mode }: { colorMode: { toggleColorMode: () => void; }, mode: PaletteMode }) => {
     return (
         <>
