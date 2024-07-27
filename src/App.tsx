1b56ecb337e2b1441b318b9c796c553c7d4046e7
Francisco Paulino Arruda Filho <fpaulinofilho04@gmail.com>
2024-07-27 08:48:15 -0300

    Removendo código de testes comentado

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,6 @@
 import 'dayjs/locale/pt-br';
 import { RouterProvider } from 'react-router-dom';
 import router from './router/router';
-/*import { auth } from '../firebase/firebase.ts'
-import { signInWithEmailAndPassword } from 'firebase/auth'
-
-signInWithEmailAndPassword(auth, 'test@mail', 'testpassword')
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user
-    console.log(user)
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code
-    const errorMessage = error.message
-    console.log(errorCode, errorMessage)
-  })*/
 
 function App() {
   return (
